3adb64db499a1563988bd2f7e8b4cf57e1204091
Cameron Roe <cameron.b.roe@gmail.com>
2014-01-19 10:07:27 +0000

    Adding English words Finished generator logic

--- src/Logatome/Generator.php
+++ src/Logatome/Generator.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * @package Logatome
- */
+
 namespace Logatome;
 
 /**
@@ -20,11 +18,13 @@ namespace Logatome;
  * @version dev
  */
 class Generator {
-    /** @var int Seed for logatome generation */ private $seed;
-    /** @var string Filename for words to use for generation */ private $wordFile;
-    /** @var array Associative arrays for letter frequencies */ private $letterFrequencies = array();
-    /** @var array Associative array for word length frequencies */ private $lengthFrequencies = array();
-    
+
+    private $seed;
+    private $wordFile;
+    private $wordList;
+    private $letterFrequencies = array();
+    private $lengthFrequencies = array();
+
     /**
      * Constructor for Generator class.
      * @param int $seed The seed for the logatome generation, this will determine the maximum length of substrings to use in algorithm
@@ -33,11 +33,127 @@ class Generator {
     public function __construct($seed = 3, $wordFile) {
         $this->seed = $seed;
         $this->wordFile = $wordFile;
+        $this->loadWordList();
+        $this->calculateLengthFrequencies();
+        for ($i = 1; $i <= $seed; $i++) {
+            $this->letterFrequencies[$i] = $this->calculateLetterFrequencies($i);
+        }
+    }
+
+    private function loadWordList() {
+        if (file_exists($this->wordFile)) {
+            $words = file_get_contents($this->wordFile);
+            $this->wordList = explode("\n", $words);
+        } else {
+            echo "The file could not be found!";
+        }
+    }
+
+    private function calculateLengthFrequencies() {
+        $minLength = 9999;
+        $maxLength = 0;
+        $wordCount = 0;
+        foreach ($this->wordList as $word) {
+            $length = strlen($word);
+            $minLength = min(array($length, $minLength));
+            $maxLength = max(array($length, $maxLength));
+            if (!isset($this->lengthFrequencies[$length])) {
+                $this->lengthFrequencies[$length] = 0;
+            }
+            $this->lengthFrequencies[$length]++; // Increment the counter for word of length n
+            $wordCount++;
+        }
+        for ($i = $minLength; $i <= $maxLength; $i++) {
+            if (isset($this->lengthFrequencies[$i])) {
+                $temp = $this->lengthFrequencies[$i];
+                $this->lengthFrequencies[$i] = $temp / $wordCount;
+            }
+        }
+    }
+
+    private function calculateLetterFrequencies($seed) {
+        $freqList = array();
+        foreach ($this->wordList as $word) {
+            for ($wordPos = 0; $wordPos <= (strlen($word) - $seed); $wordPos++) {
+                $substring = substr($word, $wordPos, $seed);
+                $letter = substr($word, $wordPos + $seed, 1);
+                if (strlen($letter) == 1) {
+                    if (!isset($freqList[$substring])) {
+                        $freqInit = array();
+                        $freqList[$substring] = $freqInit;
+                        $freqList[$substring]["total"] = 0;
+                    }
+                    if (!isset($freqList[$substring][$letter])) {
+                        $freqList[$substring][$letter] = 0;
+                    }
+                    $freqList[$substring][$letter] += 1;
+                    $freqList[$substring]["total"] += 1;
+                }
+            }
+        }
+        foreach ($freqList as $substring => $letter) {
+            $subTotal = $freqList[$substring]["total"];
+            foreach ($letter as $key => $count) {
+                if (!($key == "total")) {
+                    $freqList[$substring][$key] /= $subTotal;
+                }
+            }
+        }
+        return $freqList;
+    }
+
+    private static function generateRandomKey($freqArray) {
+        $tempList = array();
+        foreach ($freqArray as $key => $value) {
+            if (!($key == "total")) {
+                for ($i = 0; $i < ceil($value * 100); $i++) {
+                    $tempList[] = $key;
+                }
+            }
+        }
+        $index = rand(0, count($tempList) - 1);
+        return $tempList[$index];
     }
     
-    private function createDictWordLengths() {
-        
+    private function generateWordLength() {
+        return Generator::generateRandomKey($this->lengthFrequencies);
     }
+    
+    public function generateWord() {
+        $alphabet = "abcdefghijklmnopqrstuvwxyz";
+        $wordLength = $this->generateWordLength();
+        $word = "";
+        $firstLetterIndex = rand(0, strlen($alphabet) - 1);
+        $word .= $alphabet[$firstLetterIndex];
+        
+        $letterFreqGen = 1;
+        for($i = 1; $i < $wordLength; $i++) {
+            $letterFreqList = $this->letterFrequencies[$letterFreqGen];
+            if($letterFreqGen < $this->seed) {
+                $letterFreqGen++;
+            }
+            $substring = null;
+            if(strlen($word) < $this->seed) {
+                $substring = $word;
+            } else {
+                $substring = substr($word, ($i - $this->seed), $this->seed + 1);
+            }
+            if(isset($letterFreqList[$substring])) {
+                $letter = Generator::generateRandomKey($letterFreqList[$substring]);
+                $word .= $letter;
+            } else {
+                return $word;
+            }
+        }
+        return $word;
+    }
+
+    public function debug() {
+        echo $this->generateWord();
+    }
+
 }
 
+$test = new Generator(3, "EnglishWords.txt");
+$test->debug();
 ?>
